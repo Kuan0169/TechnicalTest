0d3348fd2429a893026b19a9428b3b54071c3144
Kuan <h0169888@gmail.com>
2025-06-04 22:55:41 +0200

    created entites product

---
+++ src/Core/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+        public int ProductId { get; set; }
+    }
+}
