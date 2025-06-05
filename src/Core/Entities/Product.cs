1ab97e70fcf8722366b0b7331da410d3c6fe50f0
Kuan <h0169888@gmail.com>
2025-06-05 16:48:20 +0200

    updated enities item CreatedAt

--- src/Core/Entities/Product.cs
+++ src/Core/Entities/Product.cs
@@ -11,6 +11,7 @@ namespace Core.Entities
         public string? Description { get; set; }
         [Range(0, double.MaxValue)]
         public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public int ProductId { get; set; }
     }
 }
